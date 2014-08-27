6973a128350baf87c7fe29a4716fe7f1334566ee
Damien Sawyer <damiensawyer@gmail.com>
2014-08-28 00:04:09 +1000

    initial

---
+++ XamarinFormsReactiveUIExample/Forms/MyPage/MyPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using ReactiveUI;
+using ReactiveUI.XamForms;
+
+namespace XamarinFormsReactiveUIExample
+{
+	public partial class MyPage : ReactiveContentPage<MyPageModel>
+	{
+		public MyPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ XamarinFormsReactiveUIExample/Forms/MyPage/MyPageModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ReactiveUI;
+
+namespace XamarinFormsReactiveUIExample
+{
+	public class MyPageModel : ReactiveObject
+	{
+		public MyPageModel ()
+		{
+
+		}
+
+		// see http://jen20.com/blog/2013/05/29/reactive-ui-part-2-inotifypropertychanged/
+		// apparently we can't have lower case _firstName?
+
+		private string _userName = "";
+
+		public string UserName {
+			get { return _userName; }
+			set { this.RaiseAndSetIfChanged (ref _userName, value); }
+		}
+
+		private string _password = default(string);
+
+		public string Password {
+			get { return _password; }
+			set { this.RaiseAndSetIfChanged (ref _password, value); }
+		}
+
+	}
+}
+
