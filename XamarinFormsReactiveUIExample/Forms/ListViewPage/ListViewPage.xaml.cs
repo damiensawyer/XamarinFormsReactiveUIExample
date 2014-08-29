8faa255eea8ea6bed1eac32ab3d49fcaac8f0170
Damien Sawyer <damiensawyer@gmail.com>
2014-08-29 22:49:27 +1000

    Object for use in the list

--- XamarinFormsReactiveUIExample/Forms/ListViewPage/ListViewPage.xaml.cs
+++ XamarinFormsReactiveUIExample/Forms/ListViewPage/ListViewPage.xaml.cs
@@ -18,6 +18,7 @@ namespace XamarinFormsReactiveUIExample
             this.myCollection.ItemsSource = this.ViewModel.myCollection;
             this.BindCommand(ViewModel, x => x.Add, v => v.add);
             this.BindCommand(ViewModel, x => x.Remove, v => v.remove);
+            this.BindCommand(ViewModel, x => x.Add100, v => v.add100);
             //this.OneWayBind(ViewModel, vm => vm.myCollection, v => v.myCollection.ItemsSource);
         }
     }
--- XamarinFormsReactiveUIExample/Forms/ListViewPage/ListViewPageModel.cs
+++ XamarinFormsReactiveUIExample/Forms/ListViewPage/ListViewPageModel.cs
@@ -1,35 +1,60 @@
 ﻿using System;
+using System.Linq;
+using System.Windows.Input;
 using ReactiveUI;
 
 namespace XamarinFormsReactiveUIExample
 {
-	public class ListViewPageModel : ReactiveObject
-	{
-		private string _userName = "";
-        public ReactiveList<string> myCollection { get; private set; }
+    public class ListViewPageModel : ReactiveObject
+    {
+        private string _userName = "";
+        public ReactiveList<RowModel> myCollection { get; private set; }
         public ReactiveCommand<Object> Add { get; private set; }
         public ReactiveCommand<Object> Remove { get; private set; }
+        public ReactiveCommand<Object> Add100 { get; private set; }
 
-	    public ListViewPageModel()
-	    {
-	        this.myCollection = new ReactiveList<string>();
-            myCollection.Add("Cathryn");
-            myCollection.Add("Damien");
-            myCollection.Add("Matthew");
-            myCollection.Add("Peter");
-
+        public ListViewPageModel()
+        {
+            this.myCollection = new ReactiveList<RowModel>();
+            var i = 0;
+            myCollection.Add(new RowModel {Name = "Cathryn", Index = i++});
+            myCollection.Add(new RowModel {Name = "Damien", Index = i++});
+            myCollection.Add(new RowModel {Name = "Matthew", Index = i++});
+            myCollection.Add(new RowModel {Name = "Peter", Index = i++});
 
             //Add = this.WhenAnyValue(x => x.Players.Count, x => x.NewPlayerName,
             //(count, newPlayerName) => count < 7 && !string.IsNullOrWhiteSpace(newPlayerName) && !this.Players.Contains(newPlayerName))
             //.ToCommand();
 
-	        this.Add = ReactiveCommand.Create();
-            this.Add.Subscribe(x => this.myCollection.Add(DateTime.Now.ToString()));
+            this.Add = ReactiveCommand.Create();
+            this.Add.Subscribe(x => this.AddDate());
 
             this.Remove = ReactiveCommand.Create();
-            this.Remove.Subscribe(x => this.myCollection.RemoveAt(myCollection.Count -1));
+            this.Remove.Subscribe(x => this.myCollection.RemoveAt(myCollection.Count - 1));
 
-	    }
-	}
+            this.Add100 = ReactiveCommand.Create();
+            this.Add100.Subscribe(x =>
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    this.AddDate();
+                }
+
+            });
+        }
+
+        public void AddDate()
+        {
+            this.myCollection.Add(new RowModel {Name = DateTime.Now.ToString(), Index = myCollection.Count});
+        }
+
+
+
+        public class RowModel
+        {
+            public string Name { get; set; }
+            public int Index { get; set; }
+        }
+    }
 }
 
